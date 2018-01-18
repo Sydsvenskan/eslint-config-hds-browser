ecf1b3383587c615ac89c84f07270c474e559aab
Pelle Wessman <pelle@kodfabrik.se>
2018-01-18 14:40:04 +0100

    Fix rules assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Hack around the fact that the es6 environment isn't overrideable in ESLint
 
-const config = Object.create(require('eslint-config-semistandard'));
+const config = Object.assign({}, require('eslint-config-semistandard'));
 
 config.env = {
   browser: true,
@@ -13,11 +13,5 @@ config.parserOptions = {
   ecmaVersion: 5,
   sourceType: 'script'
 };
-
-config.rules = Object.create(config.rules);
-
-config.rules['semi'] = [2, 'always'];
-config.rules['no-extra-semi'] = 2;
-config.rules['semi-spacing'] = [2, { 'before': false, 'after': true }];
 
 module.exports = config;
