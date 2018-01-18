21a9fc6da061ddf631b285256eecb3f6d468f81a
Pelle Wessman <pelle@kodfabrik.se>
2018-01-18 15:48:57 +0100

    Add compat linting + tweak other linting

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 
 const config = Object.assign({}, require('eslint-config-semistandard'));
 
+config.plugins = [].concat(config.plugins || [], ['unicorn', 'compat']);
+config.extends = [].concat(config.extends || [], 'plugin:unicorn/recommended');
+
 config.env = {
   browser: true,
   commonjs: true
@@ -13,5 +16,12 @@ config.parserOptions = {
   ecmaVersion: 5,
   sourceType: 'script'
 };
+
+config.rules = Object.assign({}, config.rules, {
+  'no-multi-spaces': ['error', {'ignoreEOLComments': true}],
+  'unicorn/explicit-length-check': 0,
+  'unicorn/no-fn-reference-in-iterator': 0,
+  'compat/compat': 'error'
+});
 
 module.exports = config;
