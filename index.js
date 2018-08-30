98cf4dda00e13448dd3c95ae9f3286513295d9f1
Pelle Wessman <pelle@kodfabrik.se>
2018-08-30 15:25:36 +0200

    Only warn about catch error var name

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ config.rules = Object.assign({}, config.rules, {
   'no-console': 'warn',
   'unicorn/explicit-length-check': 0,
   'unicorn/no-fn-reference-in-iterator': 0,
-  'unicorn/catch-error-name': ['error', { 'name': 'err' }],
+  'unicorn/catch-error-name': [1, { 'name': 'err' }],
   'compat/compat': 'error'
 });
 
