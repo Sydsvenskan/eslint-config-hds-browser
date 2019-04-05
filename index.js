1797db3f43b62d800b2609b800ed1f3ecd3f7b70
Pelle Wessman <pelle@kodfabrik.se>
2019-04-05 22:17:02 +0200

    Have also unicorn/prefer-node-remove be a warning

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ config.rules = Object.assign({}, config.rules, {
   'unicorn/prefer-add-event-listener': 1,
   'unicorn/prefer-spread': 1,
   'unicorn/prefer-node-append': 1,
+  'unicorn/prefer-node-remove': 1,
   'unicorn/prefer-includes': 1,
   'unicorn/prevent-abbreviations': 0,
   'compat/compat': 'error'
