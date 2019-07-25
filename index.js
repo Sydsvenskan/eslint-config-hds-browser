ed4af0057a84331d2795ae8cbc8c3c6350e64160
Pelle Wessman <pelle@kodfabrik.se>
2019-07-25 16:08:45 +0200

    Update + sync + sort rules

--- index.js
+++ index.js
@@ -26,25 +26,28 @@ config.parserOptions = {
 };
 
 config.rules = Object.assign({}, config.rules, {
-  'semi': [2, 'always'],
-  'no-extra-semi': 2,
+  'compat/compat': 'error',
   'func-style': [1, 'expression', { 'allowArrowFunctions': true }],
+  'no-console': 'warn',
+  'no-extra-semi': 2,
   'no-multi-spaces': ['error', { 'ignoreEOLComments': true }],
   'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
-  'no-console': 'warn',
+  'no-unused-vars': [2, { 'args': 'after-used', 'ignoreRestSiblings': true }],
   'no-var': 2,
   'object-shorthand': [2, 'properties'],
+  'quote-props': [2, 'as-needed', { 'keywords': true, 'numbers': true, 'unnecessary': false }],
+  'semi': [2, 'always'],
   'unicorn/catch-error-name': [1, { 'name': 'err' }],
   'unicorn/explicit-length-check': 0,
   'unicorn/no-fn-reference-in-iterator': 0,
-  'unicorn/prefer-exponentiation-operator': 0,
+  'unicorn/no-for-loop': 0,
   'unicorn/prefer-add-event-listener': 1,
-  'unicorn/prefer-spread': 1,
+  'unicorn/prefer-exponentiation-operator': 0,
+  'unicorn/prefer-includes': 1,
   'unicorn/prefer-node-append': 1,
   'unicorn/prefer-node-remove': 1,
-  'unicorn/prefer-includes': 1,
-  'unicorn/prevent-abbreviations': 0,
-  'compat/compat': 'error'
+  'unicorn/prefer-spread': 1,
+  'unicorn/prevent-abbreviations': 0
 });
 
 module.exports = config;
