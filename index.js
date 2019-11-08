ccf6eec05fbf2889174deb33613472f06fb87121
Pelle Wessman <pelle.wessman@hdsydsvenskan.se>
2019-11-08 15:46:17 +0100

    Deactivate unicorn/prefer-reflect-apply

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ config.rules = Object.assign({}, config.rules, {
   'unicorn/prefer-includes': 1,
   'unicorn/prefer-node-append': 1,
   'unicorn/prefer-node-remove': 1,
+  'unicorn/prefer-reflect-apply': 0,
   'unicorn/prefer-spread': 1,
   'unicorn/prevent-abbreviations': 0
 });
